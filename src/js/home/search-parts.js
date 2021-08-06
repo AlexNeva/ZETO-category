af36271e7911695661fcc2081d3810e26683006f
AlexNeva <sashnevski@yandex.ru>
2021-08-06 20:01:25 +0300

    cards + accordions

--- src/js/home/search-parts.js
+++ src/js/home/search-parts.js
@@ -1,13 +1,13 @@
 import Choices from "choices.js";
 
 export default function initSearchPartsSelects() {
-  const selects = document.querySelectorAll('.search-parts__select');
+  const selects = document.querySelectorAll('.select-primary');
 
   selects.forEach(select => {
     const choices = new Choices(select, {
       itemSelectText: '',
       shouldSort: false,
-
+      searchEnabled: select.classList.contains('no-search') ? false : true,
     });
 
   })
